f52d846938e7d18e8168ba6f8ac70309201338b4
KirillGluhov <valeralunin34@gmail.com>
2023-05-09 00:16:01 +0700

    Разделитель и начата написание класса Block

--- app/src/main/java/com/example/newapp/ActivityWithMainPage.kt
+++ app/src/main/java/com/example/newapp/ActivityWithMainPage.kt
@@ -1,11 +1,18 @@
 package com.example.newapp
-import android.os.Bundle
 
+import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+
 
 class ActivityWithMainPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+
+
     }
+
+
 }
